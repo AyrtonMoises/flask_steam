9ef49a0f0831b111d1fe1f8bb46890e59f707e9d
ayrtonmoises <ayrton.sousa@gmail.com>
2022-07-20 11:04:47 -0300

    Adicionado o Sentry para monitoramento

--- flask_steam/app.py
+++ flask_steam/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_steam.ext import configuration
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from dynaconf import settings
 
 
 load_dotenv()
 
 def minimal_app(**config):
+    sentry_sdk.init(
+        dsn=settings('SENTRY_DSN'),
+        integrations=[
+            FlaskIntegration(),
+        ],
+        traces_sample_rate=1.0
+    )
+
     app = Flask(__name__)
     configuration.init_app(app, **config)
     return app
